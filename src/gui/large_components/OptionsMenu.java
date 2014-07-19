e1754aa3aade82dd77998f686cf014c91bd1ed5b
Brett Fox <bsf7@duke.edu>
2014-07-19 00:27:49 -0400

    Fixed naming in options menu

--- src/gui/large_components/OptionsMenu.java
+++ src/gui/large_components/OptionsMenu.java
@@ -5,6 +5,7 @@ import gui.FullRecipeGUI;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -19,22 +20,32 @@ import javax.swing.JOptionPane;
  */
 public class OptionsMenu extends JMenuBar {
 
-	private static final String NEW_NAME_CONSTANT = "New";
+	private static final String CLOSE_DECISION_QUESTION = "Are you sure you want to close?";
+	private static final String EXIT_MENU_ITEM_NAME = "Exit";
+	private static final String NEW_MENU_ITEM_NAME = "New";
 	private static final String FILE_NAME_CONSTANT = "File";
 
-	private FullRecipeGUI myFullRecipeGUI;
-	public OptionsMenu(FullRecipeGUI gui) {
+	private JFrame myFullRecipeGUI;
+
+	/**
+	 * JMenuBar for Recipe Creator. It houses all of the options bars that exist
+	 * at the top of the component.
+	 * 
+	 * @param gui
+	 *            GUI that this bar exists in
+	 */
+	public OptionsMenu(JFrame gui) {
 		myFullRecipeGUI = gui;
 		createMenuBar();
 	}
 
-	public void createMenuBar() {
+	private void createMenuBar() {
 		add(createFileMenu());
 	}
 
 	private JMenu createFileMenu() {
 		JMenu fileMenu = new JMenu(FILE_NAME_CONSTANT);
-		JMenuItem newItem = new JMenuItem(NEW_NAME_CONSTANT);
+		JMenuItem newItem = new JMenuItem(NEW_MENU_ITEM_NAME);
 		newItem.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -42,22 +53,23 @@ public class OptionsMenu extends JMenuBar {
 			}
 
 		});
-		
-		JMenuItem exitItem = new JMenuItem("Exit");
+
+		JMenuItem exitItem = new JMenuItem(EXIT_MENU_ITEM_NAME);
 		exitItem.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				int closeDecision = JOptionPane.showConfirmDialog(myFullRecipeGUI, "Are you sure you want to close?");
-				if (closeDecision == JOptionPane.YES_OPTION){
+				int closeDecision = JOptionPane.showConfirmDialog(
+						myFullRecipeGUI, CLOSE_DECISION_QUESTION);
+				if (closeDecision == JOptionPane.YES_OPTION) {
 					myFullRecipeGUI.dispose();
 				}
 			}
-			
+
 		});
 
 		fileMenu.add(newItem);
 		fileMenu.add(exitItem);
 		return fileMenu;
 	}
-	
+
 }
